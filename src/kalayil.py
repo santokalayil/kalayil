c94d93eacc66db7be462938fb4e6b41bebbb0550
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2020-01-16 03:52:20 +0530

    version 0.1.4

--- src/kalayil.py
+++ src/kalayil.py
@@ -468,17 +468,3 @@ if __name__ == '__main__':
 			Mannarakulanji, 
 			Pathanamthitta
 		''')
-
-if __name__ == '__main__':
-	print('''
-		=================================
-		Module Written by Santo K. Thomas
-		=================================
-		email: santokalayil@gmail.com
-		phone: +91 8891960880
-		Address:
-			Kalayil House
-			Cheenkalthadom P.O,
-			Mannarakulanji, 
-			Pathanamthitta
-		''')
