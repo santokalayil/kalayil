e5793e97c4f2a869c4525da327435aa4236dbc99
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2020-01-15 23:12:36 +0530

    Adding First Section of files

--- README.md
+++ README.md
@@ -15,10 +15,10 @@ pip install santa
 '''python
 from santa import modelling as sanm
 
-# Rull All Machine Learning Classificatin alogorithms at once and see the results
+> Rull All Machine Learning Classificatin alogorithms at once and see the results
 RunAll(models,data,phase='',scores=None,trained_models = {},use_params=False)
 
-# Feature Scale your data OHE_features refers to one hot encoded features. OHE_features are to be listed to avoid them
+> Feature Scale your data OHE_features refers to one hot encoded features. OHE_features are to be listed to avoid them
 FeatureScale(data,OHE_features,scaler='MinMaxScaler')
 
 
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md",'r') as fh:
+	long_description = fh.read()
+
+setup(
+	name='santa',
+	version='0.1.0', # PEP440
+	description='Package for easier Machine Learning Workflow.',
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	py_modules=['santa'], # by Aus
+	package_dir ={'':'src'}, # by Aus.. BUT create a src folder in root foler inside which put my py file
+
+	author='Santo K Thomas',
+	author_email='santokalayil@gmail.com',
+	classifiers={
+			'Development Status :: 3 - Beta',
+			'Intended Audience :: Data Scientists',
+			'License :: OSI Approved :: MIT License',
+			'Programming Language :: Python :: 3',
+			'Programming Language :: Python :: 3.7',
+			'Programming Language :: Python :: 3.8',
+			'Operating System :: OS Independent'
+	},
+	)
