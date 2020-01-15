64a2423280220529285c8b54d1f1ed24472d9c87
Santo K Thomas <49450970+santokalayil@users.noreply.github.com>
2020-01-15 23:20:34 +0530

    resolving

---
+++ src/santa.py
@@ -0,0 +1,17 @@
+#from santa_modelling import *
+
+
+
+if __name__ == '__main__':
+	print('''
+		=================================
+		Module Written by Santo K. Thomas
+		=================================
+		email: santokalayil@gmail.com
+		phone: +91 8891960880
+		Address:
+			Kalayil House
+			Cheenkalthadom P.O,
+			Mannarakulanji, 
+			Pathanamthitta
+		''')
